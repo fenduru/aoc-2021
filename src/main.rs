1b79d39aedaa6be59321ef678f970e41aff1affb
Josh Fee <joshfee@gmail.com>
2021-12-08 00:02:46 -0500

    day 1b complete

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use solutions::day_1a;
 use std::fs;
 
 fn main() {
-    let input_1a = fs::read_to_string("./data/1a.txt").expect("Could not read input for 1a");
-    println!("Day 1a answer: {}", day_1a::solution(&input_1a));
+    let input_1 = fs::read_to_string("./data/1a.txt").expect("Could not read input for 1a");
+
+    println!("Day 1a answer: {}", day_1a::solution_a(&input_1));
+    println!("Day 1b answer: {}", day_1a::solution_b(&input_1));
 }
--- src/solutions/day_1a.rs
+++ src/solutions/day_1a.rs
@@ -1,46 +1,64 @@
-use itertools::Itertools;
-
-fn get_increase_frequency<T: PartialOrd>(values: &[T]) -> usize {
+fn count_increases<T: PartialOrd>(values: &[T]) -> usize {
     values
-        .iter()
-        .tuple_windows()
-        .filter(|(first, second)| second > first)
+        .windows(2)
+        .filter(|window| match window {
+            [first, second] => second > first,
+            _ => false,
+        })
         .count()
 }
 
-fn numeric_lines(input: &str) -> Vec<usize> {
+fn smooth_data(values: &[u32], samples: usize) -> Vec<f64> {
+    values
+        .windows(samples)
+        .map(|window| f64::from(window.iter().sum::<u32>()) / (window.len() as f64))
+        .collect()
+}
+
+fn numeric_lines(input: &str) -> Vec<u32> {
     input.lines().filter_map(|line| line.parse().ok()).collect()
 }
 
-pub fn solution(input: &str) -> usize {
-    get_increase_frequency(&numeric_lines(input))
+pub fn solution_a(input: &str) -> usize {
+    count_increases(&numeric_lines(input))
+}
+
+pub fn solution_b(input: &str) -> usize {
+    let smoothed_data = smooth_data(&numeric_lines(input), 3);
+    count_increases(&smoothed_data)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::solutions::day_1a::get_increase_frequency;
+    use crate::solutions::day_1a::{count_increases, smooth_data};
 
     #[test]
     fn increasing() {
         let input = [1, 2, 3];
-        assert_eq!(get_increase_frequency(&input), 2)
+        assert_eq!(count_increases(&input), 2)
     }
 
     #[test]
     fn decreasing() {
         let input = [3, 2, 1];
-        assert_eq!(get_increase_frequency(&input), 0)
+        assert_eq!(count_increases(&input), 0)
     }
 
     #[test]
     fn plateaus() {
         let input = [1, 2, 2, 3];
-        assert_eq!(get_increase_frequency(&input), 2)
+        assert_eq!(count_increases(&input), 2)
     }
 
     #[test]
     fn mixed() {
         let input = [1, 2, 3, 2, 3, 2];
-        assert_eq!(get_increase_frequency(&input), 3)
+        assert_eq!(count_increases(&input), 3)
+    }
+
+    #[test]
+    fn smoothed() {
+        let input = [1, 5, 3, 4];
+        assert_eq!(smooth_data(&input, 3), [3.0, 4.0])
     }
 }
